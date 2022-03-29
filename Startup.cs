e2d37291e899dd583a85e649f485625e86da063d
NoemiAmori <noemiamorim910@gmail.com>
2022-03-29 01:28:12 -0300

    Habilitando biblioteca de log

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Meadote
 {
@@ -26,7 +27,7 @@ namespace Meadote
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
@@ -48,6 +49,8 @@ namespace Meadote
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            loggerFactory.AddFile("Logs/meulog-{Date}.txt", LogLevel.Trace);
         }
     }
 }
